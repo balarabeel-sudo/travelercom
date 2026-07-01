d0847a587dea20d43d0cf38c328bde7aed908e6d
balarabeel-sudo <balarabeel@gmail.com>
2026-07-01 12:26:09 +0100

    fix register import

--- src/Ragister.tsx
+++ src/Ragister.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
-import { supabase } from './supabaseclient'
+import { supabase } from './supabaseClient'
 
 function Register() {
   const navigate = useNavigate()
