4791a1bedaa629e6526f130f55816a273b20f69f
balarabeel-sudo <balarabeel@gmail.com>
2026-07-03 16:19:46 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,6 @@
 
 import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+const supabaseUrl = https://hmxmuxrylzslkdybxyuv.supabase.co
+const supabaseKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhteG11eHJ5bHpzbGtkeWJ4eXV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0MTg5NDYsImV4cCI6MjA5Nzk5NDk0Nn0.nI9zRkRa0uA0Tn9q92lCZ7wwgo3bJw9wl_Vz_AdasqI
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
